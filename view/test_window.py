de9625b1214e537685a7c2277b0f79f7b42728e5
Isaac Dobbins <35647734+idobbins@users.noreply.github.com>
2020-10-07 02:45:04 -0600

    printed docstrings

---
+++ view/test_window.py
@@ -0,0 +1,48 @@
+"""
+test_window.py
+"""
+
+import pytest
+import dummy
+
+import tkinter as tk
+from window import Window
+
+
+@pytest.fixture(scope="module")
+def empty_window():
+    """returns an empty window with bse size 800 x 600"""
+    app = Window(tk.Tk())
+
+    return app
+
+
+def test_window_init(empty_window):
+    """validate that the Window object is able to init"""
+    assert isinstance(empty_window, Window)
+    assert isinstance(empty_window.master, tk.Tk)
+
+
+def test_window_dimensions(empty_window):
+    """assert initial window size is correct"""
+    assert empty_window.width == 800
+    assert empty_window.height == 600
+
+
+def test_window_set_size(empty_window):
+    """test window resize method"""
+    new_size = [600, 400]
+
+    empty_window.size = new_size
+    assert new_size == empty_window.size
+
+
+def test_window_get_nodes(empty_window):
+    """test get_nodes"""
+    pass
+
+
+def test_window_mainloop(empty_window):
+    """simple test to ensure mainloop works"""
+    empty_window.after(500, empty_window.master.destroy)
+    empty_window.mainloop()
--- view/window.py
+++ view/window.py
@@ -3,14 +3,52 @@ Root window creation logic. this can effectively be a singleton unless we
 want to have multiple windows open, though that is not likely. Call once
 On application init
 """
+# TODO: implement singleton logic
 
-'''
-Window class: responsible for being the 'root' component of all other widgets,
-layout, etc.. has properties like height, width, etc. will most likely handle
-resize events as well.
-'''
+import tkinter as tk
 
-'''
-window class init function. Needs to create the window of course, and set up
-any window-level logic data pertinent to the view.
-'''
+
+class Window(tk.Frame):
+    """
+    Window class: responsible for being the 'root' component of all other
+    widgets, layout, etc.. has properties like height, width, etc. will most
+    likely handle resize events as well.
+    """
+
+    def __init__(self, master=None, title: str = "Test Window",
+                 width: int = 800, height: int = 600):
+        """
+        window class init function. Needs to create the window of course,
+        and set up any window-level logic data pertinent to the view.
+        """
+
+        super().__init__(master)
+        self.pack()
+
+        self.width = width
+        self.height = height
+        self.set_size()
+
+        self.master.title(title)
+
+        self.nodes = []
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def get_nodes(self):
+        return self.nodes
+
+    @property
+    def size(self):
+        return [self.width, self.height]
+
+    @size.setter
+    def size(self, value: list):
+        """manually resize window within setter"""
+        self.width = value[0]
+        self.height = value[1]
+        self.set_size()
+
+    def set_size(self):
+        self.master.geometry(str(self.width) + "x" + str(self.height))
