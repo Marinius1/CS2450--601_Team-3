21d7f078cae6e5d1f9cc80095934fab575afb009
Isaac Dobbins <35647734+idobbins@users.noreply.github.com>
2020-10-06 22:36:07 -0600

    added docstrings for style class

---
+++ view/Style/style.py
@@ -0,0 +1,10 @@
+"""
+class to encapsulate the color config data. will take a color scheme as input
+and populate the values based on what is in the file. will likely utilize
+something close to the itermcolor format because it is well defined
+"""
+
+'''
+init(config: str): void
+load the correct color config file, assign values to pre-determined constants
+'''
