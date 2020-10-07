c512e83bf86e8631a09360f7503dca2401a4bc4c
Marinius1 <45081283+Marinius1@users.noreply.github.com>
2020-10-06 21:40:19 -0600

    View (#13)
    * added view files, no docstrings yet, just added files. this is all according to the current class diagram for the view.
    
    * initial commit, did some work before the restructure but it is all here now.
    
    Co-authored-by: Isaac Dobbins <35647734+idobbins@users.noreply.github.com>

---
+++ view/Input/checkbox.py
@@ -0,0 +1,26 @@
+"""
+checkbox input widget. stores a boolean value that is displaed via a
+checked/not checked box.
+"""
+
+'''
+init(name: str, flag=false: bool): void
+sets default value to false unless otherwise specified
+'''
+
+'''
+do(): void
+class level implementation of this method. emits proper signal to controller.
+also calls swap().
+'''
+
+'''
+click(): void
+event function bound to checkbox click event.
+'''
+
+'''
+swap(): void
+toggles the flag data member. true->false or false->true
+'''
+
