4a6036faee75fe0672b2fe62cc3583c63e0213eb
Isaac Dobbins <35647734+idobbins@users.noreply.github.com>
2020-11-06 23:21:19 -0700

    ttk scrollbar, table refactor

--- View/people_v0.py
+++ View/people_v0.py
@@ -51,25 +51,46 @@ class People(UINode):
                              focuscolor=self.colors.a10
                              )
 
-        self.style.map('People.Vertical.TScrollbar')
-        self.style.configure('People.Vertical.TScrollbar',
-                             background=self.colors.a7,
-                             activebackground=self.colors.a8,
-                             troughcolor=self.colors.background,
-                             borderwidth=0,
-                             relief=tk.FLAT,
-                             activerelief=tk.FLAT,
-                             )
-
         self.home_frame = tk.Frame(self.master)
-        self.home_frame.configure(background=self.colors.background, border=3, relief=tk.RIDGE)
+        self.home_frame.configure(background=self.colors.background, border=3,
+                                  relief=tk.RIDGE)
         self.home_frame.grid(row=1, column=0, sticky=tk.NSEW)
 
-        self.home_frame.rowconfigure(0, weight=1)
+        self.home_frame.rowconfigure(0, weight=0)
+        self.home_frame.rowconfigure(1, weight=1)
+        # self.home_frame.columnconfigure(1, weight=1)
 
-        self.scrollbar = ttk.Scrollbar(self.home_frame, style='People.Vertical.TScrollbar')
+        # self.header = tk.Frame(self.home_frame)
+        # self.header.configure(background=self.colors.background, border=3,
+        #                       relief=tk.FLAT)
+        # self.header.grid(row=0, column=0, sticky=tk.N + tk.E + tk.W)
 
-        self.data_columns = []
+        # self.recent_actions_label = ttk.Label(self.header,
+        #                                       text="First Name",
+        #                                       style='Recent.TLabel')
+        # self.recent_actions_label.grid(row=0, column=0, sticky=tk.NW, padx=15)
+
+        # self.table = tk.Frame(self.home_frame)
+        # self.table.configure(background='red', border=3,
+        #                      relief=tk.FLAT)
+        # self.table.grid(row=1, column=0, sticky=tk.NSEW)
+        '''
+        self.column = tk.Listbox(self.table,
+                                                 foreground=self.colors.foreground,
+                                                 selectforeground=self.colors.background,
+                                                 background=self.colors.background,
+                                                 relief=tk.FLAT)
+        self.recent_actions_listbox.grid(row=0, column=0, sticky=tk.NS, padx=(15, 15))
+
+        for i in range(500):
+            self.recent_actions_listbox.insert(tk.END, "Jacob")
+
+        self.table = tk.Scrollbar(self.table)
+        self.table.grid(row=1, column=1, sticky=tk.NS)
+
+        self.column.configure(yscrollcommand=self.recent_actions_scrollbar.set)
+        self.recent_actions_scrollbar.configure(command=self.recent_actions_listbox.yview)
+        '''
 
         model_example = [
             # ['bob', 'cindy', 'lue', 'greg'],
@@ -82,44 +103,32 @@ class People(UINode):
 
         headers_example = ['First Name', 'Last Name', 'Wage']
 
+        self.listboxes = []
+
+        self.scrollbar = tk.Scrollbar(self.home_frame)
+        # self.b1 = Listbox(master, yscrollcommand=scrollbar.set)
+        # self.b2 = Listbox(master, yscrollcommand=scrollbar.set)
+        self.scrollbar.config(command=self.yview)
+        # self.b1.pack(side=LEFT, fill=BOTH, expand=1)
+        # self.b2.pack(side=LEFT, fill=BOTH, expand=1)
+
         self.create_table(headers_example, model_example)
-        self.scrollbar.configure(command=self.yview)
+        self.scrollbar.grid(row=0, column=5)
 
     def yview(self, *args):
 
-        for i in self.data_columns:
-            i.yview(*args)
-
-            pos = i.yview()
-            self.scrollbar.set(pos[0], pos[1])
-
-    def sync_yview(self, *args):
-
-        for i in range(len(self.data_columns)):
-            data_columns_copy = self.data_columns.copy()
-            data_columns_copy.pop(i)
-
-            sync_check = [self.data_columns[i].yview() == j.yview() for j in data_columns_copy]
-
-            if not any(sync_check):
-                for k in range(len(self.data_columns)):
-                    if k != i:
-                        self.data_columns[k].yview_moveto(args[0])
-                    self.scrollbar.set(*args)
+        for i in self.listboxes:
+            i.yview()
 
     def create_table(self, lyst1, lyst2):
 
-        lyst1_size = len(lyst1)
-
-        for i in range(lyst1_size):
+        for i in range(len(lyst1)):
 
             self.home_frame.columnconfigure(i, weight=0)
 
-            grid_frame = tk.Frame(self.home_frame, background='red')
+            grid_frame = tk.Frame(self.home_frame,
+                                  background=self.colors.background)
             grid_frame.grid(row=0, column=i, sticky=tk.NS)
-
-            grid_frame.rowconfigure(0, weight=0)
-            grid_frame.rowconfigure(1, weight=1)
 
             button = ttk.Button(grid_frame, text=lyst1[i],
                                 style='Header.TButton')
@@ -130,21 +139,22 @@ class People(UINode):
                                 selectforeground=self.colors.background,
                                 background=self.colors.background,
                                 relief=tk.FLAT,
-                                yscrollcommand=self.sync_yview)
+                                yscrollcommand=self.scrollbar.set)
             column.grid(row=1, column=0, sticky=tk.NS)
 
             for j in range(len(lyst2[i])):
                 column.insert(tk.END, lyst2[i][j])
 
-                if j % 2 == 0:
+                if (j % 2 == 0):
                     background = self.colors.background
                 else:
                     background = self.colors.a7
 
                 column.itemconfigure(j, background=background)
-            # column.configure(height=len(lyst2[i]))
+            column.configure(height=len(lyst2[i]))
 
-            self.data_columns.append(column)
+            self.listboxes.append(column)
 
-        self.home_frame.columnconfigure(lyst1_size + 1, weight=1)
-        self.scrollbar.grid(row=0, column=lyst1_size + 1, sticky=tk.N+tk.S+tk.E)
+    def resize_summary_frame(self, event, title, content):
+        title.configure(font=('Roboto', int(event.height * .1)))
+        content.configure(font=('Roboto', int(event.height * .2), 'bold'))
