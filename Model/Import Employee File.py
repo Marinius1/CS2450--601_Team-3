b6f4e91ea1702b88eaa860a13408fa09faa6cf8f
Marinius1 <45081283+Marinius1@users.noreply.github.com>
2020-11-08 00:33:05 -0700

    Model controller v0.3 (#23)
    * Changes to imports, button commands, and beginnings of data aggrigation from view to model via the controller.
    
    * -Worked on employ import from file.
    
    * -Worked on employ import from file.
    
    Co-authored-by: Sean <samcnees@gmail.com>

--- Model/Import Employee File.py
+++ Model/Import Employee File.py
@@ -1,27 +1,89 @@
+import json
+import random
 
-def import_employee_file():
-    writefile = open('data.txt', 'a')
-    file = str(input(("Enter file name here:")))
-    with open(file, 'r') as f:
-        for line in f:
-            employeelist = []
-            line = line.strip()
-            employeelist.append(line)
-            id = employeelist.pop(0)
-            id = id.split(',')
-            payType = id.pop(7)
-            name = id.pop(0)
+EMPLOYEE_FILE = "employees.csv"
+
+class addToEmployeeFile:
+
+    def __init__(self, number, first, last, type, amount, address, state, city, zip, bDay, social, pNumber):
+        self.pNumber = pNumber
+        self.social = social
+        self.bDay = bDay
+        self.number = number
+        self.first = first
+        self.last = last
+        self.type = type
+        self.amount = amount
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+
+    def add_to_employee_file(self):
+        with open('dataFile.json') as infile:
+            data1 = json.load(infile)
+            data1.append(
+                {"Employee number": self.number, "First name": self.first, "Last name": self.last,
+                     "Address": self.address, "City": self.city, "State": self.state,
+                      "Pay type": self.type, "Pay amount": self.amount})
 
 
-            if payType == '1':
-                paytype = "salary"
-            elif payType == '2':
-                paytype = "Commission"
-            elif payType == '3':
-                paytype = "hourly"
+        with open('dataFile.json', 'w') as outfile:
+            json.dump(data1, outfile)
 
-            print("*Birth date should be in format mm/dd/yyyy*")
-            x = str(input("Enter Birth Date:"))
 
-            data = {"Employee number": id, "Name": name, "Birth date": x,"Pay type": paytype, "Pay amount": pay_amount()}
+def phone():
+    a = random.randint(100,999)
+    b = random.randint(100,999)
+    c = random.randint(1000,9999)
+    return str(a)+"-"+str(b)+"-"+str(c)
 
+def birth():
+    a = random.randint(1, 12)
+    b = random.randint(1, 30)
+    c = random.randint(1965, 2004)
+    return str(a) + "/" + str(b) + "/" + str(c)
+
+def social():
+    a = random.randint(100, 999)
+    b = random.randint(10, 99)
+    c = random.randint(1000, 9999)
+    return str(a) + "-" + str(b) + "-" + str(c)
+
+def load_employees():
+    with open(EMPLOYEE_FILE) as empFile:
+        item = empFile.readlines()
+        item.pop(0)
+        for emp in item:
+            employeeFields = emp.rstrip().split(',')
+            empId = employeeFields[0]
+            firstName = employeeFields[1]
+            lastName = employeeFields[2]
+            address = employeeFields[3]
+            city = employeeFields[4]
+            state = employeeFields[5]
+            empZip = employeeFields[6]
+            classification = employeeFields[7]
+            salary = employeeFields[8]
+            commission = employeeFields[9]
+            hourly = employeeFields[10]
+        if classification == "3":  # hourly
+            empClass = "Hourly"
+            empClassification = (str(hourly))
+        elif classification == "1":  # salary
+            empClass = "Salary"
+            empClassification = (str(salary))
+        else:  # classification
+            empClass = "Commission"
+            empClassification = (str(commission))
+
+        print((str(empId), str(firstName), str(lastName),str(empClass),str(empClassification),
+                                str(address),str(state),str(city),str(empZip), str(birth()), str(social()),str(phone())))
+
+''''
+        i = addToEmployeeFile(str(empId), str(firstName), str(lastName),str(empClass),str(empClassification),
+                                str(address),str(state),str(city),str(empZip), str(birth()), str(social()),str(phone()))
+        i.add_to_employee_file()
+'''
+
+load_employees()
