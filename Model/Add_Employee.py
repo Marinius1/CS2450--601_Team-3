a24e69fb8d4eb73c77346933070d215d1931fd72
Sean <samcnees@gmail.com>
2020-11-14 20:35:10 -0700

    Checks if employee exists in file before adding. Raises exception if there is an employee with same name and employee number

--- Model/Add_Employee.py
+++ Model/Add_Employee.py
@@ -30,28 +30,45 @@ class add_employee:
 
     def add_to_employee_file(self):
         data = []
+        j = True
         with open('Model/employee_file.json') as infile:
             data1 = json.load(infile)
-            if len(data1) > 0:
-                data1.append({"Employee number": self.eNum,"First name": self.first, "Last name": self.last, "Phone": self.phone,
-                              "Address": self.address, "City": self.city, "State": self.state, "Social security": self.social,
-                              "Birth day": self.bDay, "Birth month": self.bMonth, "Birth year": self.bYear, "Pay type": self.type,
-                              "Pay amount": self.amount,"Start day": self.sDay, "Start month": self.sMonth, "Start year": self.sYear ,
-                              "Zip": self.zip, "Role": self.role, "Position": self.pos, "Team": self.team})
-            else:
-                data.append({"Employee number": self.eNum,"First name": self.first, "Last name": self.last, "Phone": self.phone,
-                              "Address": self.address, "City": self.city, "State": self.state, "Social security": self.social,
-                              "Birth day": self.bDay, "Birth month": self.bMonth, "Birth year": self.bYear, "Pay type": self.type,
-                              "Pay amount": self.amount,"Start day": self.sDay, "Start month": self.sMonth, "Start year": self.sYear ,
-                              "Zip": self.zip, "Role": self.role, "Position": self.pos, "Team": self.team})
+
+            for i in data1:
+                if self.eNum == i['Employee number'] and self.first == i["First name"] and self.last == i["Last name"]:
+                    j = False
+
+            if j == True:
+
+                if len(data1) > 0:
+                    data1.append({"Employee number": self.eNum,"First name": self.first, "Last name": self.last, "Phone": self.phone,
+                                          "Address": self.address, "City": self.city, "State": self.state, "Social security": self.social,
+                                          "Birth day": self.bDay, "Birth month": self.bMonth, "Birth year": self.bYear, "Pay type": self.type,
+                                          "Pay amount": self.amount,"Start day": self.sDay, "Start month": self.sMonth, "Start year": self.sYear ,
+                                          "Zip": self.zip, "Role": self.role, "Position": self.pos, "Team": self.team})
+                    with open('Model/employee_file.json', 'w') as outfile:
+                        json.dump(data1, outfile)
+
+                else:
+                    data.append({"Employee number": self.eNum,"First name": self.first, "Last name": self.last, "Phone": self.phone,
+                                          "Address": self.address, "City": self.city, "State": self.state, "Social security": self.social,
+                                          "Birth day": self.bDay, "Birth month": self.bMonth, "Birth year": self.bYear, "Pay type": self.type,
+                                          "Pay amount": self.amount,"Start day": self.sDay, "Start month": self.sMonth, "Start year": self.sYear ,
+                                          "Zip": self.zip, "Role": self.role, "Position": self.pos, "Team": self.team})
+                    with open('Model/employee_file.json', 'w') as outfile:
+                        json.dump(data, outfile)
+
+            elif j == False:
+                raise Exception("Employee with that name and employee number already exists.")
 
 
-        with open('Model/employee_file.json', 'w') as outfile:
-            json.dump(data1,outfile)
 
-employee_num = "77-1234567"
-first = "Bilbo"
-last = "Baggins"
+
+
+'''
+employee_num = "38-2406306"
+first = "Assy"
+last = "Shaxby"
 phone = "385-225-8880"
 pay_type = "Salary"
 pay_amount = "3.00"
@@ -74,3 +91,4 @@ a = add_employee(employee_num, first, last, pay_type, pay_amount, address,state,
                  phone, zzip, birth_day, birth_month, birth_year, start_day, start_month, start_year, role, position, teams)
 
 a.add_to_employee_file()
+'''
