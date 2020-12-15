37e54d3c18527a913cf6e1db6be9e74e1be671e4
Sean <10872289@my.uvu.edu>
2020-12-15 13:34:30 -0700

    Fixed payroll for employees with no hours in timecard

--- Model/payRoll.py
+++ Model/payRoll.py
@@ -14,6 +14,7 @@ class payRoll:
     def payroll(self, fyle):
         with open(fyle, 'w') as f1:
             for i in self.data:
+                f1.write('\n')
                 f1.write(i["Employee number"])
                 f1.write(" ")
                 f1.write(i["First name"])
@@ -22,16 +23,17 @@ class payRoll:
                 f1.write(" ")
                 f1.write("Pay for this period:")
                 k = i["Timecard"].strip('][').split(', ')
-                r = [float(ele) for ele in k]
-                hours = (sum(r))
-
-                if i["Pay type"] == "Hourly":
-                    f1.write(str(round(float(i["Hourly"])*hours,2)))
-                elif i["Pay type"] == "Salary":
-                    f1.write(str(round(float(i["Salary"])/2,2)))
-                elif i["Pay type"] == "Commission":
-                    f1.write(str(round((float(i["Commission"]) * (hours/100)) + float(i["Salary"])/24,2)))
-                f1.write('\n')
+                if len(k) > 1:
+                    r = [float(ele) for ele in k]
+                    hours = (sum(r))
+                    if i["Pay type"] == "Hourly":
+                        f1.write(str(round(float(i["Hourly"])*hours,2)))
+                    elif i["Pay type"] == "Salary":
+                        f1.write(str(round(float(i["Salary"])/2,2)))
+                    elif i["Pay type"] == "Commission":
+                        f1.write(str(round((float(i["Commission"]) * (hours/100)) + float(i["Salary"])/24,2)))
+                else:
+                    continue
 
 
 
