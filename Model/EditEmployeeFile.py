3637bc5aed6639a6aca4b6a83ec018985958c5f7
Jacob Jenson <jacob.jenson6@gmail.com>
2020-12-15 08:53:09 -0700

    EditThis respawn

--- Model/EditEmployeeFile.py
+++ Model/EditEmployeeFile.py
@@ -78,6 +78,25 @@ def edit_employee(empID, change, info):
         with open('Model/employee_file.json', 'w') as outfile:
             json.dump(data, outfile)
 
+
+class editThis:
+
+    def __init__(self, data):
+        self.data = data
+
+    def edit(self):
+        with open("Model/employee_file.json") as file:
+            data = json.load(file)
+            for i in self.data:
+                empID = str(i["Employee number"])
+                for j in data:
+                    if j["Employee number"] == empID:
+                        j["PTO total"] = i["PTO total"]
+                        j["PTO used"] = i["PTO used"]
+                        j["Hours/sales"] = i["Hours/sales"]
+
+        with open("Model/employee_file.json", 'w') as f:
+            json.dump(data, f)
 '''
 edit_employee("51-4678119", "First name", "Isaac")
 archive_employee("51-4678119")
